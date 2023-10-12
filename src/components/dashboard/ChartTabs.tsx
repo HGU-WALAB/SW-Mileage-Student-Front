0dc09388fa2ab0f4816cd3ef1d02af5c487c1604
ohinhyuk <8156217@naver.com>
2023-10-13 05:37:09 +0900

    style: 마일리지 차트 종류들을 구분해주는 Tabs를 구현(#6)

---
+++ src/components/dashboard/ChartTabs.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function ChartTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="총점 비교" {...a11yProps(0)} />
+          <Tab label="총점 순위" {...a11yProps(1)} />
+          <Tab label="항목 별 학생 수" {...a11yProps(2)} />
+          <Tab label="항목 별 포인트" {...a11yProps(3)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        총점 비교
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        총점 순위
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        항목 별 학생 수
+      </CustomTabPanel>
+
+      <CustomTabPanel value={value} index={3}>
+        항목 별 포인트
+      </CustomTabPanel>
+    </Box>
+  );
+}
