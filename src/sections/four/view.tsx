28a10b1874b7e0b5494d60c8d28e9f55ebb15b22
Inhyuk Oh <49269218+ohinhyuk@users.noreply.github.com>
2023-10-13 14:23:00 +0900

    FEATURE : 자신의 프로필을 나타내고 자신이 등록된 마일리지를 시각화 할 수 있다.  (#16)
    * Chore : 깃허브 이슈 템플릿 , PR 템플릿 제작
    
    * fix: 헤더에 불필요한 기능 (다국어 , 연락기능 ) 제거 (#5)
    
    * style: 사이드 바 컴포넌트 생성 및 페이지 연결(#10)
    
    * style: 마일리지 차트 종류들을 구분해주는 Tabs를 구현(#6)
    
    * feat: 나의 마일리지 총합과 학생들의 평균 마일리지 총합을 학기별로 비교하는 그래프(#6)
    
    * feat: 5개의 타입에 대한 마일리지 총합을 학생마다 내림차순으로 제시한다.(#6)
    
    * feat: 항목별 학생 수를 선 그래프로 제시 (#6)
    
    * style: 차트들에 네이밍을 붙여줌(#6)
    
    * style: 모든 차트 크기 조정 width 1000 , height 400 고정(#6)
    
    * style: 나의 마일리지 기록을 MUI DataGrip을 사용하여 조회할 수 있도록한다.(#8)
    
    * Fix: 배포 시 warning 때문에 CI가 실패하는 에러 수정
    
    * Fix : 빌드시 모든  warning 삭제
    
    * style: 학부생의 각 항목들에 대한 정보를 타입 별로 그룹핑 하고 이를 radar Chart로 가시화 해서 나타낸다.(#8)
    
    * Feat : 학부생 프로필 와이어프레임 구성

--- src/sections/four/view.tsx
+++ src/sections/four/view.tsx
@@ -5,16 +5,142 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 // components
 import { useSettingsContext } from 'src/components/settings';
+import { ResponsiveRadar } from '@nivo/radar';
+import { Paper } from '@mui/material';
 
 // ----------------------------------------------------------------------
-
+const data = [
+  {
+    taste: 'Type A',
+    // chardonay: 25,
+    // carmenere: 111,
+    point: 73,
+  },
+  {
+    taste: 'Type B',
+    // chardonay: 88,
+    // carmenere: 108,
+    point: 45,
+  },
+  {
+    taste: 'Type C',
+    // chardonay: 49,
+    // carmenere: 63,
+    point: 34,
+  },
+  {
+    taste: 'Type D',
+    // chardonay: 109,
+    // carmenere: 102,
+    point: 113,
+  },
+  {
+    taste: 'Type E',
+    // chardonay: 51,
+    // carmenere: 42,
+    point: 98,
+  },
+];
 export default function FourView() {
   const settings = useSettingsContext();
 
+  const studentInfo = [
+    '오인혁',
+    '재학중',
+    '전산전자공학부',
+    '4학년',
+    '8학기',
+    '21800446',
+    '컴퓨터공학',
+    '컴퓨터공학',
+  ];
+
+  const studentInfoTitle = ['이름', '재학여부', '학부', '학년', '학기', '학번', '전공1', '전공2'];
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
-      <Typography variant="h4"> Page Four </Typography>
+      <Typography variant="h4"> 학부생 프로필 </Typography>
 
+      <Box
+        sx={{
+          mt: 5,
+          width: 1,
+          height: 320,
+          borderRadius: 2,
+          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
+          border: (theme) => `dashed 1px ${theme.palette.divider}`,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            width: '600px',
+            px: 5,
+          }}
+        >
+          <Box
+            sx={{
+              width: '250px',
+              height: '250px',
+              border: 1,
+              borderColor: 'lightGray',
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'grid',
+            gap: '30px',
+            gridTemplateColumns: 'repeat(2, 1fr)',
+          }}
+        >
+          {studentInfoTitle.map((title, index) => (
+            <Box sx={{ fontSize: '20px', display: 'flex', gap: '20px' }} key={index}>
+              <Box sx={{ fontWeight: 'bold' }}>{title}</Box> {studentInfo[index]}
+            </Box>
+          ))}
+        </Box>
+      </Box>
+
+      <Box sx={{ width: '500px', height: '500px' }}>
+        <ResponsiveRadar
+          data={data}
+          keys={['point']}
+          indexBy="taste"
+          valueFormat=">-.2f"
+          margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+          borderColor={{ from: 'color' }}
+          gridLabelOffset={36}
+          dotSize={10}
+          dotColor={{ theme: 'background' }}
+          dotBorderWidth={2}
+          colors={{ scheme: 'nivo' }}
+          blendMode="multiply"
+          motionConfig="wobbly"
+          legends={[
+            {
+              anchor: 'top-left',
+              direction: 'column',
+              translateX: -50,
+              translateY: -40,
+              itemWidth: 80,
+              itemHeight: 20,
+              itemTextColor: '#999',
+              symbolSize: 12,
+              symbolShape: 'circle',
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemTextColor: '#000',
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </Box>
       <Box
         sx={{
           mt: 5,
