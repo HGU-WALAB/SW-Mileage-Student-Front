c0328fe7297bdd2d231718e62093e3eed08d931a
ohinhyuk <8156217@naver.com>
2023-10-26 14:11:48 +0900

    Style: 나의 마일리지 학기별 조회를 위해 학기 셀렉터 컴포넌트 추가(#19(

--- src/sections/five/view.tsx
+++ src/sections/five/view.tsx
@@ -4,15 +4,21 @@ import Typography from '@mui/material/Typography';
 // components
 import { useSettingsContext } from 'src/components/settings';
 import MyMileageTable from 'src/components/dashboard/MyMileageTable';
+import SemesterDropdown from 'src/components/common/SemesterDropdown';
+import { Box } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
 export default function FiveView() {
   const settings = useSettingsContext();
 
+  const semesters = ['2022-01', '2022-02', '2023-01', '2023-02'];
+
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
       <Typography variant="h4"> 나의 마일리지 </Typography>
+      <Box sx={{ height: '20px' }} />
+      <SemesterDropdown semesters={semesters} />
       <MyMileageTable />
     </Container>
   );
