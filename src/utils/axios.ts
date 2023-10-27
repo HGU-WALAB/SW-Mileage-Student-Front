0dbc60592e0d011563daf2979a51207aeab472cc
ohinhyuk <8156217@naver.com>
2023-10-27 09:26:12 +0900

    Feat: axios 인스턴스에 토큰 넣는것 설정(#27)

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -11,6 +11,14 @@ axiosInstance.interceptors.response.use(
   (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
 );
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default axiosInstance;
 
 // ----------------------------------------------------------------------
