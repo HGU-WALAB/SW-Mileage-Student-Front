d5107b750be8aaa889ff271d518223971257134e
ohinhyuk <8156217@naver.com>
2023-11-02 08:09:05 +0900

    Feat: 신청 기간이라면 디데이와 시작 , 끝일을 계산해줌(#31)

--- src/utils/converter/dateConverter.ts
+++ src/utils/converter/dateConverter.ts
@@ -8,3 +8,26 @@ export const dateConverter = (date: string) => {
   //   const second = dateObj.getSeconds();
   return `${year}년 ${month}월 ${day}일 ${hour}시`;
 };
+
+export function parseMonthAndDay(dateTimeStr: string) {
+  const match = dateTimeStr.match(/(\d{4})-(\d{2})-(\d{2})T/);
+  if (match) {
+    const [, , month, day] = match;
+    return `${parseInt(month, 10)}월 ${parseInt(day, 10)}일`;
+  }
+  return null;
+}
+
+export function daysBetween(date1Str: string, date2Str: string) {
+  // 날짜 문자열을 Date 객체로 변환
+  const date1 = new Date(date1Str);
+  const date2 = new Date(date2Str);
+
+  // 두 날짜의 차이를 밀리초로 계산
+  const diffInMilliseconds = Math.abs(date2.getTime() - date1.getTime());
+
+  // 밀리초를 일로 변환
+  const diffInDays = diffInMilliseconds / (1000 * 60 * 60 * 24);
+
+  return Math.round(diffInDays);
+}
